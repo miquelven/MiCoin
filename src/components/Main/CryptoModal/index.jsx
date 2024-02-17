e3bd0382113e4c2b137a7353b2097c413fcb39a8
miquelven <miquelven.silva@gmail.com>
2024-02-17 10:30:05 -0300

    criação do grafico

--- src/components/Main/CryptoModal/index.jsx
+++ src/components/Main/CryptoModal/index.jsx
@@ -1,8 +1,9 @@
-import { useContext, useEffect, useLayoutEffect, useState } from "react";
-import ReactDOM from "react-dom";
-import { useNavigate, useParams } from "react-router-dom";
-import { CryptoContext } from "../../../context/CryptoContext";
-import { Triangle } from "lucide-react";
+import { useContext, useEffect, useLayoutEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { CryptoContext } from '../../../context/CryptoContext';
+import { Triangle } from 'lucide-react';
+import Chart from './Chart';
 
 const Indicator = ({ currentPrice, high, low }) => {
   const [greenColor, setGreenColor] = useState();
@@ -15,16 +16,10 @@ const Indicator = ({ currentPrice, high, low }) => {
 
   return (
     <>
-      <span
-        className="bg-red-600 h-1.5 rounded-l-lg w-[50%]"
-        style={{ width: `${100 - greenColor}%` }}
-      >
+      <span className="bg-red-600 h-1.5 rounded-l-lg w-[50%]" style={{ width: `${100 - greenColor}%` }}>
         &nbsp;
       </span>
-      <span
-        className="bg-green-600 h-1.5 rounded-r-lg w-[50%]"
-        style={{ width: `${greenColor}%` }}
-      >
+      <span className="bg-green-600 h-1.5 rounded-r-lg w-[50%]" style={{ width: `${greenColor}%` }}>
         &nbsp;
       </span>
     </>
@@ -36,7 +31,7 @@ export default function CryptoModal() {
   const navigate = useNavigate();
 
   const close = () => {
-    navigate("..");
+    navigate('..');
   };
 
   const { getCoinData, coinData, currency } = useContext(CryptoContext);
@@ -58,14 +53,8 @@ export default function CryptoModal() {
           <div className="flex items-center justify-between h-full w-full p-4">
             <div className="flex flex-col w-[45%] h-full pr-2">
               <div className="flex w-full items-center">
-                <img
-                  className="w-[3rem] h-[3rem] mx-1.5"
-                  src={coinData.image.large}
-                  alt={coinData.id}
-                />
-                <h1 className="text-xl capitalize font-medium">
-                  {coinData.name}
-                </h1>
+                <img className="w-[3rem] h-[3rem] mx-1.5" src={coinData.image.large} alt={coinData.id} />
+                <h1 className="text-xl capitalize font-medium">{coinData.name}</h1>
                 <span className="text-sm py-0.5 px-2.5 ml-2 bg-blue-600 text-blue-500 bg-opacity-25 rounded uppercase">
                   {coinData.symbol}
                 </span>
@@ -73,29 +62,25 @@ export default function CryptoModal() {
               <div className="flex w-full mt-6">
                 <div className="flex flex-col w-full ">
                   <div className="flex justify-between ">
-                    <span className="text-sm capitalize text-zinc-500">
-                      Price
-                    </span>
+                    <span className="text-sm capitalize text-zinc-500">Price</span>
                     <div
                       className={`text-sm px-1 ml-2 font-medium flex items-center rounded uppercase bg-opacity-25
                     ${
                       coinData.market_data.price_change_percentage_24h > 0
-                        ? "bg-green-600 text-green-500"
-                        : "bg-red-600 text-red-500"
+                        ? 'bg-green-600 text-green-500'
+                        : 'bg-red-600 text-red-500'
                     }
                     `}
                     >
                       <span className="flex items-center gap-2">
-                        {Number(
-                          coinData.market_data.price_change_percentage_24h
-                        ).toFixed(2)}
+                        {Number(coinData.market_data.price_change_percentage_24h).toFixed(2)}
                         %
                         <Triangle
                           className={`w-3 h-4 
                         ${
                           coinData.market_data.price_change_percentage_24h > 0
-                            ? "fill-green-600"
-                            : "fill-red-600 rotate-180"
+                            ? 'fill-green-600'
+                            : 'fill-red-600 rotate-180'
                         }
                         `}
                         />
@@ -103,8 +88,8 @@ export default function CryptoModal() {
                     </div>
                   </div>
                   <h2 className="text-2xl font-bold">
-                    {new Intl.NumberFormat("en-IN", {
-                      style: "currency",
+                    {new Intl.NumberFormat('en-IN', {
+                      style: 'currency',
                       currency: currency,
                       maximumSignificantDigits: 5,
                     }).format(coinData.market_data.current_price[currency])}
@@ -113,40 +98,32 @@ export default function CryptoModal() {
               </div>
               <div className="flex w-full mt-4 justify-between">
                 <div className="flex flex-col">
-                  <span className="text-sm capitalize text-zinc-500">
-                    Market Cap
-                  </span>
+                  <span className="text-sm capitalize text-zinc-500">Market Cap</span>
                   <h2 className="text-lg font-bold">
-                    {new Intl.NumberFormat("en-IN", {
-                      style: "currency",
+                    {new Intl.NumberFormat('en-IN', {
+                      style: 'currency',
                       currency: currency,
                       minimumFractionDigits: 0,
                     }).format(coinData.market_data.market_cap[currency])}
                   </h2>
                 </div>
                 <div className="flex flex-col">
-                  <span className="text-sm capitalize text-zinc-500">
-                    fully diluted valuation
-                  </span>
+                  <span className="text-sm capitalize text-zinc-500">fully diluted valuation</span>
                   <h2 className="text-lg font-bold">
-                    {new Intl.NumberFormat("en-IN", {
-                      style: "currency",
+                    {new Intl.NumberFormat('en-IN', {
+                      style: 'currency',
                       currency: currency,
-                      notation: "compact",
-                    }).format(
-                      coinData.market_data.fully_diluted_valuation[currency]
-                    )}
+                      notation: 'compact',
+                    }).format(coinData.market_data.fully_diluted_valuation[currency])}
                   </h2>
                 </div>
               </div>
 
               <div className="flex flex-col w-full mt-4 justify-between">
-                <span className="text-sm capitalize text-zinc-500">
-                  total volume
-                </span>
+                <span className="text-sm capitalize text-zinc-500">total volume</span>
                 <h2 className="text-lg font-bold">
-                  {new Intl.NumberFormat("en-IN", {
-                    style: "currency",
+                  {new Intl.NumberFormat('en-IN', {
+                    style: 'currency',
                     currency: currency,
                     minimumFractionDigits: 0,
                   }).format(coinData.market_data.total_volume[currency])}
@@ -163,24 +140,20 @@ export default function CryptoModal() {
 
               <div className="flex w-full mt-4 justify-between">
                 <div className="flex flex-col">
-                  <span className="text-sm capitalize text-zinc-500">
-                    Low 24H
-                  </span>
+                  <span className="text-sm capitalize text-zinc-500">Low 24H</span>
                   <h2 className="text-lg font-bold">
-                    {new Intl.NumberFormat("en-IN", {
-                      style: "currency",
+                    {new Intl.NumberFormat('en-IN', {
+                      style: 'currency',
                       currency: currency,
                       minimumFractionDigits: 5,
                     }).format(coinData.market_data.low_24h[currency])}
                   </h2>
                 </div>
                 <div className="flex flex-col">
-                  <span className="text-sm capitalize text-zinc-500">
-                    high 24H
-                  </span>
+                  <span className="text-sm capitalize text-zinc-500">high 24H</span>
                   <h2 className="text-lg font-bold">
-                    {new Intl.NumberFormat("en-IN", {
-                      style: "currency",
+                    {new Intl.NumberFormat('en-IN', {
+                      style: 'currency',
                       currency: currency,
                       minimumFractionDigits: 5,
                     }).format(coinData.market_data.high_24h[currency])}
@@ -190,24 +163,21 @@ export default function CryptoModal() {
 
               <div className="flex w-full mt-4 justify-between">
                 <div className="flex flex-col">
-                  <span className="text-sm capitalize text-zinc-500">
-                    max suply
-                  </span>
+                  <span className="text-sm capitalize text-zinc-500">max suply</span>
                   <h2 className="text-lg font-bold">
-                    {new Intl.NumberFormat("en-IN", {
-                      style: "currency",
+                    {new Intl.NumberFormat('en-IN', {
+                      style: 'currency',
                       currency: currency,
                       minimumFractionDigits: 0,
-                    }).format(coinData.market_data.max_supply)}
+                    }).format(coinData.market_data.max_supply
+                      )}
                   </h2>
                 </div>
                 <div className="flex flex-col">
-                  <span className="text-sm capitalize text-zinc-500">
-                    circulating supply
-                  </span>
+                  <span className="text-sm capitalize text-zinc-500">circulating supply</span>
                   <h2 className="text-lg font-bold">
-                    {new Intl.NumberFormat("en-IN", {
-                      style: "currency",
+                    {new Intl.NumberFormat('en-IN', {
+                      style: 'currency',
                       currency: currency,
                       minimumFractionDigits: 0,
                     }).format(coinData.market_data.circulating_supply)}
@@ -216,9 +186,7 @@ export default function CryptoModal() {
               </div>
 
               <div className="flex flex-col content-start mt-5">
-                <span className="text-sm capitalize text-zinc-500">
-                  sentiment
-                </span>
+                <span className="text-sm capitalize text-zinc-500">sentiment</span>
                 <div>
                   <div className="flex flex-col w-24 my-2 ">
                     <div
@@ -227,9 +195,7 @@ export default function CryptoModal() {
                     `}
                     >
                       <span className="flex items-center gap-2">
-                        {Number(coinData.sentiment_votes_up_percentage).toFixed(
-                          2
-                        )}
+                        {Number(coinData.sentiment_votes_up_percentage).toFixed(2)}
                         %
                         <Triangle
                           className={`w-3 h-4 fill-green-600
@@ -245,9 +211,7 @@ export default function CryptoModal() {
                     `}
                     >
                       <span className="flex items-center gap-2">
-                        {Number(
-                          coinData.sentiment_votes_down_percentage
-                        ).toFixed(2)}
+                        {Number(coinData.sentiment_votes_down_percentage).toFixed(2)}
                         %
                         <Triangle
                           className={`w-3 h-4 
@@ -260,11 +224,24 @@ export default function CryptoModal() {
                 </div>
               </div>
             </div>
-            <div className="flex flex-col w-[55%] h-full pr-3"></div>
+            <div className="flex flex-col w-[55%] h-full pr-3">
+              <Chart id={coinData.id} />
+
+              <div className='flex justify-end mt-4'>
+                <h3 className='text-zinc-50 text-xl py-1'>
+                  <span className='text-zinc-600 capitalize mr-1'>
+                    market cap rank: 
+                  </span>
+                    <span>
+                    {coinData.market_cap_rank}
+                    </span>
+                </h3>
+              </div>
+            </div>
           </div>
         ) : null}
       </div>
     </div>,
-    document.getElementById("model")
+    document.getElementById('model'),
   );
 }
