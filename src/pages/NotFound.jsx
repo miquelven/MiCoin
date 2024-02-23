f8131f30b3ac4faecbc7eec33a46976b469d5197
miquelven <miquelven.silva@gmail.com>
2024-02-23 16:14:26 -0300

    criação da page notFound

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="fixed inset-0 flex justify-center items-center z-[1] ">
+      <h3 className="text-5xl text-zinc-800 dark:text-zinc-200 max-sm:text-3xl max-[380px]:text-2xl">
+        {" "}
+        404 - Page not found
+      </h3>
+    </div>
+  );
+}
