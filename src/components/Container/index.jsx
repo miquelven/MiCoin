0b9d9b94dd8f57c9f36508b3061854194fc9b417
miquelven <miquelven.silva@gmail.com>
2024-03-18 16:26:55 -0300

    remoção de um comentario

--- src/components/Container/index.jsx
+++ src/components/Container/index.jsx
@@ -7,5 +7,5 @@ export default function Container({ children }) {
     Aos.init();
   }, []);
 
-  return <div className="max-w-6xl m-auto px-5 ">{children}</div>; // Renderize as props
+  return <div className="max-w-6xl m-auto px-5 ">{children}</div>;
 }
