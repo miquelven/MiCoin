18a3d81cb8d9d968992fe5c6180ed42dd8b8c940
miquelven <miquelven.silva@gmail.com>
2024-05-02 12:36:07 -0300

    feat: create features itens data

---
+++ src/data/featuresItens.jsx
@@ -0,0 +1,26 @@
+import { Filter, Info, Star, ThumbsUp } from "lucide-react";
+
+const featuresItens = [
+  {
+    id: 0,
+    icon: <Star size={"60px"} />,
+    text: "Keep your favorite cryptocurrencies at your fingertips. Easily bookmark them with a click for quick and easy access.",
+  },
+  {
+    id: 1,
+    icon: <ThumbsUp size={"60px"} />,
+    text: "Discover the top-performing cryptocurrencies. See the best investment options at a glance.",
+  },
+  {
+    id: 2,
+    icon: <Filter size={"60px"} />,
+    text: "Customize your search. Use our filtering options to find cryptocurrencies that fit your investment profile.",
+  },
+  {
+    id: 3,
+    icon: <Info size={"60px"} />,
+    text: "Explore comprehensive data. Get detailed information on each cryptocurrency, including price history and market analysis.",
+  },
+];
+
+export default featuresItens;
