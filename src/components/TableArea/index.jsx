20992cef2d7f14aa423b374ec4b3db4293575241
miquelven <miquelven.silva@gmail.com>
2024-05-02 12:35:52 -0300

    feat: create table area component

---
+++ src/components/TableArea/index.jsx
@@ -0,0 +1,17 @@
+import Table from "../Table";
+import FilterTable from "../Table/FilterTable";
+
+export default function TableArea() {
+  return (
+    <section className="flex flex-col justify-center items-center mt-10 max-md:mt-36">
+      <h2
+        data-aos="fade-up"
+        className="mb-10 text-4xl font-semibold max-sm:text-2xl"
+      >
+        Market Overview
+      </h2>
+      <FilterTable />
+      <Table />
+    </section>
+  );
+}
