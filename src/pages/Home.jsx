a40d4e3ecee33d31911087edce5f732c52d456a3
miquelven <miquelven.silva@gmail.com>
2024-05-02 18:07:27 -0300

    refactor: comment content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,20 +3,22 @@ import { useEffect } from "react";
 import Banner from "../components/Banner";
 import TableArea from "../components/TableArea";
 import FeaturesArea from "../components/FeaturesArea";
+import CryptoText from "../components/CryptoText";
 
 function Home() {
-  useEffect(() => {
-    const isCoin = localStorage.getItem("coin") || [];
-    if (isCoin.length > 0) return;
+  // useEffect(() => {
+  //   const isCoin = localStorage.getItem("coin") || [];
+  //   if (isCoin.length > 0) return;
 
-    localStorage.setItem("coin", JSON.stringify([]));
-  }, []);
+  //   localStorage.setItem("coin", JSON.stringify([]));
+  // }, []);
 
   return (
     <>
       <Banner />
       <TableArea />
       <FeaturesArea />
+      <CryptoText />
       <Outlet />
     </>
   );
