1d254bbebf88c08a5a2c3077b170be20bda3d530
miquelven <miquelven.silva@gmail.com>
2024-05-02 18:06:33 -0300

    style: change margin

--- src/components/FeaturesArea/index.jsx
+++ src/components/FeaturesArea/index.jsx
@@ -2,7 +2,7 @@ import featuresItens from "../../data/featuresItens";
 
 export default function FeaturesArea() {
   return (
-    <section className=" min-h-80  flex justify-center items-center md:mt-20 md:mb-32">
+    <section className=" min-h-80 mt-40  flex justify-center items-center md:my-40 shadow-md shadow-zinc-300 dark:shadow-black rounded-lg">
       <div className="rounded-lg bg-zinc-200 dark:bg-zinc-900/30  w-screen p-10 grid   md:grid-cols-2 xl:grid-cols-4 gap-10 ">
         {featuresItens.map((item) => (
           <div
@@ -10,7 +10,7 @@ export default function FeaturesArea() {
             className="flex flex-col justify-center items-center gap-10 px-10 h-full dark:even:bg-[#09090b] even:bg-[#f4f4f5] rounded-lg py-4"
           >
             <span>{item.icon}</span>
-            <p className="text-sm md:text-md text-center">{item.text}</p>
+            <p className="text-sm md:text-base text-center">{item.text}</p>
           </div>
         ))}
       </div>
