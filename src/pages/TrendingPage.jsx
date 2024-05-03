1f65e4a6d1fa59af878a2cd72a08973ba329a360
miquelven <miquelven.silva@gmail.com>
2024-05-03 14:43:43 -0300

    refactor: fix text title page

--- src/pages/TrendingPage.jsx
+++ src/pages/TrendingPage.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 import useTitle from "../hooks/useTitle";
 
 export default function TrendingPage() {
-  useTitle("Trending7");
+  useTitle("Trending");
 
   return (
     <>
