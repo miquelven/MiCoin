eb1b256a538c6b6409c4dc66329b76e675a45268
miquelven <miquelven.silva@gmail.com>
2024-05-03 09:53:52 -0300

    feat: create stepsitens data

---
+++ src/data/stepsItens.jsx
@@ -0,0 +1,29 @@
+import stepFavorite1 from "../assets/images/stepImg1.png";
+import stepFavorite2 from "../assets/images/stepImg2.png";
+import stepFavorite3 from "../assets/images/stepImg3.png";
+
+const stepsItens = [
+  {
+    id: 0,
+    stepCounter: "1- ",
+    text: "First, you should click on the star icon next to the asset you want to favorite.",
+    img: stepFavorite1,
+    alt: "Step-by-step image of how to favorite an asset",
+  },
+  {
+    id: 1,
+    stepCounter: "2- ",
+    text: "Second, navigate to the 'Favorites' tab where you can see your favorited asset.",
+    img: stepFavorite2,
+    alt: "Step-by-step image of how to favorite an asset",
+  },
+  {
+    id: 2,
+    stepCounter: "3- ",
+    text: "Third, you can now view your favorite assets.",
+    img: stepFavorite3,
+    alt: "Step-by-step image of how to favorite an asset",
+  },
+];
+
+export default stepsItens;
