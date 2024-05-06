19892b27d7110c7d65c530ff42d332d33724181a
miquelven <miquelven.silva@gmail.com>
2024-05-06 16:29:04 -0300

    style: add new animation when scroll

--- src/components/FeaturesArea/index.jsx
+++ src/components/FeaturesArea/index.jsx
@@ -4,8 +4,10 @@ export default function FeaturesArea() {
   return (
     <section className=" min-h-80 mt-40  flex justify-center items-center md:my-40 shadow-md shadow-zinc-300 dark:shadow-black rounded-lg">
       <div className="rounded-lg bg-zinc-200 dark:bg-zinc-900/30  w-screen p-10 grid   md:grid-cols-2 xl:grid-cols-4 gap-10 ">
-        {featuresItens.map((item) => (
+        {featuresItens.map((item, index) => (
           <div
+            data-aos={index % 2 == 0 ? "fade-up" : "fade-bottom"}
+            data-aos-delay={index * 300}
             key={item.id}
             className="flex flex-col justify-center items-center gap-10 px-10 h-full dark:even:bg-[#09090b] even:bg-[#f4f4f5] rounded-lg py-4"
           >
